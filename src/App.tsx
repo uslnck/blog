0666dd2bc7d85d27fd8eb2c8e98d09cb09006052
uslnck <117448466+uslnck@users.noreply.github.com>
2023-08-03 12:07:21 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,12 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.less";
 import Header from "./components/header";
 import Main from "./components/main";
-import ArticleList from "./components/article-list";
-import ArticleInside from "./components/article-inside";
-import SignIn from "./components/sign-in";
-import SignUp from "./components/sign-up";
-import UserProfile from "./components/user-profile";
-import NewArticle from "./components/new-article";
-import NotFound from "./components/not-found";
-import { useState } from "react";
-
-const pageSize = 5;
-const pagesCount = 5;
 
 export default function Blog() {
-  const [currentOffset, setCurrentOffset] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-    setCurrentOffset((page - 1) * pageSize);
-  };
-
   return (
     <>
       <Header />
       <Main />
-      <Routes>
-        {["/", "/articles"].map((path, i) => (
-          <Route
-            key={i}
-            path={path}
-            element={
-              <ArticleList
-                currentPage={currentPage}
-                currentOffset={currentOffset}
-                handlePageChange={handlePageChange}
-                pageSize={pageSize}
-                pagesCount={pagesCount}
-              />
-            }
-          />
-        ))}
-        <Route path="/articles/:slug" element={<ArticleInside />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/new-article" element={<NewArticle />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
     </>
   );
 }
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,30 +1,30 @@
-// import ArticleInside from "../article-inside";
-// import ArticleList from "../article-list";
+import ArticleInside from "../article-inside";
+import ArticleList from "../article-list";
 import styles from "./main.module.less";
-// import { Routes, Route } from "react-router-dom";
-// import NotFound from "../not-found";
-// import { useState } from "react";
-// import SignUp from "../sign-up";
-// import SignIn from "../sign-in";
-// import UserProfile from "../user-profile";
-// import NewArticle from "../new-article";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "../not-found";
+import { useState } from "react";
+import SignUp from "../sign-up";
+import SignIn from "../sign-in";
+import UserProfile from "../user-profile";
+import NewArticle from "../new-article";
 
-// const pageSize = 5;
-// const pagesCount = 5;
+const pageSize = 5;
+const pagesCount = 5;
 
 export default function Main() {
-  // const [currentOffset, setCurrentOffset] = useState(0);
-  // const [currentPage, setCurrentPage] = useState(1);
+  const [currentOffset, setCurrentOffset] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // const handlePageChange = (page: number) => {
-  //   setCurrentPage(page);
-  //   setCurrentOffset((page - 1) * pageSize);
-  // };
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    setCurrentOffset((page - 1) * pageSize);
+  };
 
   return (
     <main>
       <div className={styles.mainContainer}>
-        {/* <Routes>
+        <Routes>
           {["/", "/articles"].map((path, i) => (
             <Route
               key={i}
@@ -46,7 +46,7 @@ export default function Main() {
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/new-article" element={<NewArticle />} />
           <Route path="*" element={<NotFound />} />
-        </Routes> */}
+        </Routes>
       </div>
     </main>
   );
