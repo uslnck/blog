9ea4ef84d2e770cb7f0f54a0b5f4c3f9303bb238
uslnck <117448466+uslnck@users.noreply.github.com>
2023-09-04 12:03:49 +0300

    F

--- src/components/Like/utils.ts
+++ src/components/Like/utils.ts
@@ -1,7 +1,4 @@
 export const determineLikeDisabled = (isLoading: boolean) => {
-  console.log(!localStorage.getItem("token"));
-  console.log(isLoading);
-  console.log(!localStorage.getItem("token") || isLoading);
   const isDisabled = !localStorage.getItem("token") || isLoading;
   return isDisabled;
 };
